6de09c029ddeb015e3d10e7f33de315ec034d5cb
Administrator <Administrator@utass-mbp.lan>
2021-11-08 09:03:06 -0500

    RB score rushing yards, TD, fumbles

--- scoring.js
+++ scoring.js
@@ -1,21 +1,50 @@
-const calculateScore = (player) => {
-  if (player.position === 'QB'){
-    return calculateQBScore(player)
+const calculateScore = (knownposition) => {
+  switch (knownposition.position) {
+    case 'QB' :
+      return calculateQBScore (knownposition)
+    case 'RB' :
+      return calculateRBScore (knownposition)
+    case 'WR' :
+      return calculateWRScore (knownposition)
+    case 'TE' :
+      return calculateTEScore (knownposition)
+    default:
+      return 0
   }
 }
 
-// calculate QB Score for QB position gi
+// calculate QB Score for QB position 
 const calculateQBScore = (player) => { 
-  const score = (player.stats.passing.yards /25) + (player.stats.passing.touchdowns * 6) +
-  (player.stats.passing.interceptions * -3) + (player.stats.rushing.yards /10) +
-  (player.stats.rushing.touchdowns * 6) +(player.stats.rushing.fumbles * -3)
+  const score = (player.stats.passing.yards /25) + 
+  (player.stats.passing.touchdowns * 6) +
+  (player.stats.passing.interceptions * -3) + 
+  (player.stats.rushing.yards /10) +
+  (player.stats.rushing.touchdowns * 6) +
+  (player.stats.rushing.fumbles * -3)
+
   return score
 }
+
+// calculate RB score for RB position
+const calculateRBScore = (player) => {
+  (player.stats.rush.yards / 10) +
+  (player.stats.rush.touchdowns * 6) +
+  (player.stats.rush.fumbles * -3)
+
+  return rushing.yards + rushing.touchdowns + rushing.fumbles
+}
+
+const calculateQBScore = (player) => { 
+  const score = (player.stats.passing.yards /25) + 
+  (player.stats.passing.touchdowns * 6) +
+  (player.stats.passing.interceptions * -3) + 
+  (player.stats.rushing.yards /10) +
+  (player.stats.rushing.touchdowns * 6) +
+  (player.stats.rushing.fumbles * -3)
+
+  return score
+}
+
 module.exports = calculateScore
 
 
-
-// QB Score - comment 
-// RB Score - comment
-// WR Score - comment
-// TE Score - comment
